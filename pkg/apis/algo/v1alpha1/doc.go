59a010940f6cefd03d4a8989368af1c51955bc61
Mike Cowgill <mike@algohub.com>
2019-03-20 16:25:19 -0700

    Added endpoint api and controller

---
+++ pkg/apis/algo/v1alpha1/doc.go
@@ -0,0 +1,4 @@
+// Package v1alpha1 contains API Schema definitions for the algo v1alpha1 API group
+// +k8s:deepcopy-gen=package,register
+// +groupName=algo.run
+package v1alpha1
---
+++ pkg/apis/algo/v1alpha1/endpoint_types.go
@@ -0,0 +1,49 @@
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// EndpointSpec defines the desired state of Endpoint
+// +k8s:openapi-gen=true
+type EndpointSpec struct {
+	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
+	// Add custom validation using kubebuilder tags: https://book.kubebuilder.io/beyond_basics/generating_crd.html
+}
+
+// EndpointStatus defines the observed state of Endpoint
+// +k8s:openapi-gen=true
+type EndpointStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
+	// Add custom validation using kubebuilder tags: https://book.kubebuilder.io/beyond_basics/generating_crd.html
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// Endpoint is the Schema for the endpoints API
+// +k8s:openapi-gen=true
+type Endpoint struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   EndpointSpec   `json:"spec,omitempty"`
+	Status EndpointStatus `json:"status,omitempty"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// EndpointList contains a list of Endpoint
+type EndpointList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []Endpoint `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&Endpoint{}, &EndpointList{})
+}
