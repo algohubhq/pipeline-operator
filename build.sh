e6426066691097fc24863bacecf452acd7895c17
Mike Cowgill <mike@algohub.com>
2019-10-14 20:01:24 -0700

    Updated build script to enable gomodules
    Former-commit-id: 3d6955b7e82737ee55b3c6a8ef713c24081c2e7a

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 GIT_COMMIT=$(git rev-list -1 HEAD)
 VERSION=$(git describe --all --exact-match `git rev-parse HEAD` | grep tags | sed 's/tags\///')
 
-operator-sdk build algohub/pipeline-operator:$GIT_COMMIT
+GO111MODULE=on operator-sdk build algohub/pipeline-operator:$GIT_COMMIT
 
 docker tag algohub/pipeline-operator:$GIT_COMMIT algohub/pipeline-operator:latest
 
